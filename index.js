1db836fbdabec4d336de183ec64d58ce96b96a17
sowmik-sec <sowmik.sec@gmail.com>
2023-03-13 16:13:12 +0600

    deleting data from database

--- index.js
+++ index.js
@@ -50,6 +50,12 @@ const run = async () => {
       const result = await orderCollection.insertOne(order);
       res.send(result);
     });
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 };

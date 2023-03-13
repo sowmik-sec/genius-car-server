8fb18c094563f6c268523370cbe76610094d6c01
sowmik-sec <sowmik.sec@gmail.com>
2023-03-13 11:08:41 +0600

    basic node mongo server setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middle wares
+app.use(cors());
+app.use(express());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", (req, res) => {
+  res.send("genius car server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Genius car server is listening on port ${port}`);
+});
